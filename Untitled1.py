cd2c71b6454beb583e32ec94ee97f81fede9771d
LoayCosmo <68798475+LoayKhal@users.noreply.github.com>
2020-09-15 15:07:41 -0500

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the Capstone Notebook
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+print ('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
